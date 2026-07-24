a458909de8e5663c6798e618193a855f3e7aeb57
MeridianBC <brian.cumminger@gmail.com>
2026-07-24 11:10:51 -0300

    Expanded build process

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
+@echo off
+REM Valid options are: gui_onefile, cli_onefile, both_onedir
+set BUILD_TYPE=%1
+if "%BUILD_TYPE%"=="" set BUILD_TYPE=gui_onefile
+
+echo Starting PyInstaller with configuration: %BUILD_TYPE%
 pyinstaller --clean build.spec
