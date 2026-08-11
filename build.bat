367a9c3bafd8727d0327727851527100b92ea643
MeridianBC <brian.cumminger@gmail.com>
2026-08-11 17:14:01 -0300

    build process changes

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 echo Deleting old dist folder...
 if exist dist rmdir /s /q dist
 
@@ -7,3 +8,9 @@ pyinstaller --clean build.spec
 
 echo Copying bizhawk-connector to dist...
 xcopy /E /I /Y bizhawk-connector dist\bizhawk-connector
+
+set "PATH=%~dp0;%PATH%"
+cd dist
+7z a coop2.zip * -x!config.ini
+cd ..
+endlocal
